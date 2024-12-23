a62fa4559179fa7656882af296453ec9d887cddf
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-23 02:36:49 +0200

    update Home page styles and content for improved user experience

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -7,14 +7,17 @@ const Home = () => {
   return (
     <div className="home-page">
       <div className="home-container">
-        <h1>Manage your work anytime, anywhere.</h1>
+        <h1>Welcome to Daily To-do.</h1>
+        <h2>Simplify Your Life, One Task at a Time.</h2>
         <p>
-          Transform the way you manage tasks and projects
-          with our intuitive and powerful platform. At Tassc,
-          we streamline your workflow, enhance productivity,
-          and keep you organized every step of the way.
+        Daily Todo is your personal productivity companion, 
+        designed to help you stay organized and achieve more every day. 
+        Whether you’re managing work projects, personal errands, or 
+        lifelong goals, Daily Todo makes task management effortless 
+        and intuitive.
         </p>
         <button>Get Started</button>
+        
     </div>
     </div>
   );
