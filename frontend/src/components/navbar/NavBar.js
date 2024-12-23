e5679f72eb0952971762ff556911c8a0c682a6b2
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-23 02:36:21 +0200

    refactor NavBar component to improve layout and modal handling

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -1,45 +1,65 @@
-import React from 'react';
-import Login from "../login/Login";
-import Signup from "../signup/Signup";
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Login from '../../pages/login/Login';
+import Signup from '../../pages/signup/Signup';
+import "./NavBar.css"; 
 
 const NavBar = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+  const [isSignupModalOpen, setSignupModalOpen] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     navigate('/login');
-
-    const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-    const [isSignupModalOpen, setSignupModalOpen] = useState(false);
-  
-    const openLoginModal = () => setLoginModalOpen(true);
-    const closeLoginModal = () => setLoginModalOpen(false);
-    const openSignupModal = () => setSignupModalOpen(true);
-    const closeSignupModal = () => setSignupModalOpen(false);
   };
 
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      {token ? (
-        <>
-          <Link to="/tasks">Tasks</Link>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </>
-      )}
+  const openLoginModal = () => setLoginModalOpen(true);
+  const closeLoginModal = () => setLoginModalOpen(false);
+  const openSignupModal = () => setSignupModalOpen(true);
+  const closeSignupModal = () => setSignupModalOpen(false);
 
-          <div>
-          <button onClick={openLoginModal}>Login</button>
-          <span>Or</span>
-          <button onClick={openSignupModal}>Sign Up</button>
+  return (
+    <nav className="navbar">
+      <div className="navbar-container"> 
+
+      <div className="navbar-logo">
+        <Link to="/">
+        <img src="daily_to_do.png" alt="Daily To-Do" />
+        </Link>
         </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        {token ? (
+          <>
+            <li>
+              <Link to="/tasks">Tasks</Link>
+            </li>
+            <li>
+              <button onClick={handleLogout} className="logout-button">Logout</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <button onClick={openLoginModal} className="nav-button">Login</button>
+            </li>
+            <li>
+              <button onClick={openSignupModal} className="nav-button">Sign Up</button>
+            </li>
+          </>
+        )}
+      </ul>
+
       {/* Modals */}
       {isLoginModalOpen && (
         <div className="modal-overlay">
@@ -55,6 +75,7 @@ const NavBar = () => {
           </div>
         </div>
       )}
+      </div>
     </nav>
   );
 };
