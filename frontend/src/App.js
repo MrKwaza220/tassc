2ae7b8c54d6248de385c0813cfe1219744f7864f
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-23 02:35:55 +0200

    add NavBar component and About page to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TaskForm from './components/taskform/TaskForm';
 import TaskList from './components/tasklist/TaskList';
+import NavBar from './components/navbar/NavBar';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Home from './pages/home/Home';
-import { ToastContainer, toast } from 'react-toastify';
+import About from './pages/about/About';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/login" element={<Login />} />
@@ -18,7 +20,7 @@ const App = () => {
           <Route path="/tasks" element={<TaskList />} />
           <Route path="/add-task" element={<TaskForm />} />
         </Routes>
-        <ToastContainer />
+        <About />
       </div>
     </Router>
   );
