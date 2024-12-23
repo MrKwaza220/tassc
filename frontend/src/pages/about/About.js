e6d9cb4cb7b874c1c9634b3551724a7423fba8cf
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-23 02:36:34 +0200

    add About page with styling for improved layout

---
+++ frontend/src/pages/about/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about-page">
+            <div className="about-container">
+            <h1>About Us</h1>
+            
+            <p>Welcome to our website. We are dedicated to providing the best service possible.</p>
+          </div>
+        </div>
+    );
+};
+
+export default About;
