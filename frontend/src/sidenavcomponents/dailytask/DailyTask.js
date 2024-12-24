c5f78784c34e973cad3c127e6d5335813f80f8a6
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-25 00:47:26 +0200

    add 'Set Timer' task to DailyTask component

--- frontend/src/sidenavcomponents/dailytask/DailyTask.js
+++ frontend/src/sidenavcomponents/dailytask/DailyTask.js
@@ -8,6 +8,7 @@ const DailyTask = () => {
                 <li>Task 1</li>
                 <li>Task 2</li>
                 <li>Task 3</li>
+                <li>Set Timer</li>
             </ul>
         </div>
     );
