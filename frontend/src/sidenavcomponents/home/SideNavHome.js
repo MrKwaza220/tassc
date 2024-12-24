0a2129a706ab25abfffcb0d5fc0582fbb6edec30
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-25 00:38:28 +0200

    add SideNavHome component with styles for improved layout and presentation

---
+++ frontend/src/sidenavcomponents/home/SideNavHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SideNavHome.css';
+
+const SideNavHome = () => {
+    return (
+        <div className="side-nav-home">
+            <h2>Home</h2>
+            <div>
+                Welcome to home page!
+            </div>
+        </div>
+    );
+};
+
+export default SideNavHome;
