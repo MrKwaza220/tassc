1bc95e5c084c68cf4066dc2c632911e8e79d9e97
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-25 00:44:22 +0200

    add DailyTask component with styles for improved layout and presentation

---
+++ frontend/src/sidenavcomponents/dailytask/DailyTask.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DailyTask = () => {
+    return (
+        <div>
+            <h1>Daily Task</h1>
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default DailyTask;
