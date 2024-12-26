fdc60bd80583786bf96a60c9e0a00d7c1551250a
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-26 21:38:25 +0200

    fix: update link in Home component to point to SideNavBar

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
         lifelong goals, Daily Todo makes task management effortless 
         and intuitive.
         </p>
-        <a href="/dashboard">
+        <a href="/sidenavbar">
           <button>Get Started</button>
         </a>
     </div>
