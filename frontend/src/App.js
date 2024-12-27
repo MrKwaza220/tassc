5fe3136ccfeb580591337a5fa2d08dbe0d0a545e
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-27 21:59:08 +0200

    refactor: remove SideNavBar route and update routing to dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TaskForm from './components/taskform/TaskForm';
-import TaskList from './components/tasklist/TaskList';
 import NavBar from './components/navbar/NavBar';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Footer from './components/footer/Footer';
 import Dashboard from './pages/dashboard/Dashboard';
-import SideNavBar from './sidenavcomponents/sidenavbar/SideNavBar';
+
 
 const App = () => {
   return (
@@ -24,7 +22,7 @@ const App = () => {
             } 
             />
           <Route path="/about" element={<About />} />
-          <Route path="/sidenavbar" element={<SideNavBar />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           
         </Routes>
         
