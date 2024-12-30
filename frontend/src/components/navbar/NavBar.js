9012c4660342820ab7ce675ec637bb305e997ce1
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-30 03:37:13 +0200

    refactor: remove unused button and modal styles from NavBar component

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import { Link} from 'react-router-dom';
-import "./NavBar.css"; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-
-
-
   return (
     <nav className="navbar">
-      <div className="navbar-container"> 
-
-      <div className="navbar-logo">
-        <Link to="/">
-        <img src="daily_to_do.png" alt="Daily To-Do" />
-        </Link>
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <Link to="/">
+            <img src="daily_to_do.png" alt="Daily To-Do" />
+          </Link>
         </div>
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      
-      </ul>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
