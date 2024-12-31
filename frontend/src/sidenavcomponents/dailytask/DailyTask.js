9dd93c1142f98fc55d3817617b08ff8658c77dca
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-12-31 12:57:13 +0200

    refactor: remove hardcoded task list from DailyTask component

--- frontend/src/sidenavcomponents/dailytask/DailyTask.js
+++ frontend/src/sidenavcomponents/dailytask/DailyTask.js
@@ -6,13 +6,6 @@ const DailyTask = () => {
     return (
         <div>
             <h1>Daily Task</h1>
-            <ul>
-                <li>Task 1</li>
-                <li>Task 2</li>
-                <li>Task 3</li>
-                <li>Set Timer</li>
-
-            </ul>
             <TaskList />
         </div>
     );
