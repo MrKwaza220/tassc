f9b910665dc955cd47ad208e7f04d87e7c4f0bef
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-01-02 11:40:43 +0200

    style: enhance TaskForm layout and input handling for better usability

--- frontend/src/components/taskform/TaskForm.js
+++ frontend/src/components/taskform/TaskForm.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
 import "./TaskForm.css";
 
 const TaskForm = ({ onClose, task }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [status, setStatus] = useState("Pending");
+  const [status, setStatus] = useState("Select status");
   const [dueDate, setDueDate] = useState("");
-  const [dueTime, setDueTime] = useState("");  // New state for time
-  const [timer, setTimer] = useState("");      // New state for timer (e.g., 2 hours)
-  const [priority, setPriority] = useState("Medium");  // New state for priority
+  const [dueTime, setDueTime] = useState("");
+  const [timer, setTimer] = useState(""); // New state for timer (e.g., 2 hours)
+  const [priority, setPriority] = useState("Medium"); // New state for priority
 
   useEffect(() => {
     if (task) {
@@ -17,8 +20,8 @@ const TaskForm = ({ onClose, task }) => {
       setDescription(task.description);
       setStatus(task.status);
       setDueDate(new Date(task.dueDate).toISOString().split("T")[0]);
-      setDueTime(task.dueTime ? task.dueTime : "");  // Set time if exists
-      setTimer(task.timer ? task.timer : "");        // Set timer if exists
+      setDueTime(task.dueTime ? task.dueTime : ""); // Set time if exists
+      setTimer(task.timer ? task.timer : ""); // Set timer if exists
       setPriority(task.priority ? task.priority : "Medium"); // Set priority
     }
   }, [task]);
@@ -28,13 +31,13 @@ const TaskForm = ({ onClose, task }) => {
     const token = localStorage.getItem("token");
     console.log("Retrieved token:", token);
 
-    const taskData = { 
-      name, 
-      description, 
-      status, 
-      dueDate: `${dueDate}T${dueTime}`,  // Combine date and time
+    const taskData = {
+      name,
+      description,
+      status,
+      dueDate: `${dueDate}T${dueTime}`, // Combine date and time
       timer,
-      priority
+      priority,
     };
     console.log("Submitting task data:", taskData);
 
@@ -73,84 +76,92 @@ const TaskForm = ({ onClose, task }) => {
   return (
     <div className="modal" onClick={onClose}>
       <div className="task-form" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={onClose}>&times;</button>
+        <button className="close-button" onClick={onClose}>
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
         <h2>{task ? "Edit Task" : "Add Task"}</h2>
+
         <form onSubmit={handleSubmit}>
-          <div>
-           
-            <input
-              type="text"
-              placeholder="Enter task name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required              
-            />
+          <div className="form-group">
+            <div className="input-name">
+              <input
+                type="text"
+                placeholder="Enter task name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-description">
+              <textarea
+                type="text"
+                placeholder="Enter task description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </div>
           </div>
-          <div>
-        
-            <input
-              type="text"
-              value={description}
-              placeholder="Enter task description"
-              onChange={(e) => setDescription(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <select
-              placeholder="Select status"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-              required
-            > <option value="" disabled>Select status</option>
-              <option value="Pending">Pending</option>
-              <option value="In Progress">In Progress</option>
-              <option value="Completed">Completed</option>
-            </select>
-          </div>
-          <div>
-            <label>Due Date:</label>
-            <input
-              type="date"
-              value={dueDate}
-              onChange={(e) => setDueDate(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>Due Time:</label>
-            <input
-              type="time"
-              value={dueTime}
-              onChange={(e) => setDueTime(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Timer (hours):</label>
-            <input
-              type="number"
-              value={timer}
-              onChange={(e) => setTimer(e.target.value)}
-              min="0"
-              step="1"
-            />
-          </div>
-          <div>
-            <label>Priority:</label>
-            <select
-              value={priority}
-              onChange={(e) => setPriority(e.target.value)}
-            >
-              <option value="Urgent">Urgent</option>
-              <option value="High">High</option>
-              <option value="Medium">Medium</option>
-              <option value="Low">Low</option>
-            </select>
+          <div className="form-group-2">
+            <div className="input-group">
+              <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                required
+              >
+                {" "}
+                <option value="" disabled>
+                  Select status
+                </option>
+                <option value="Pending">Pending</option>
+                <option value="In Progress">In Progress</option>
+                <option value="Completed">Completed</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <label>Due Date:</label>
+              <input
+                type="date"
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label>Due Time:</label>
+              <input
+                type="time"
+                value={dueTime}
+                onChange={(e) => setDueTime(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label>Timer (hours):</label>
+              <input
+                type="number"
+                value={timer}
+                onChange={(e) => setTimer(e.target.value)}
+                min="0"
+                step="1"
+              />
+            </div>
+            <div className="input-group">
+              <label>Priority:</label>
+              <select
+                value={priority}
+                onChange={(e) => setPriority(e.target.value)}
+              >
+                <option value="Urgent">Urgent</option>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+            </div>
           </div>
           <button type="submit">{task ? "Update Task" : "Add Task"}</button>
-          <button type="button" className="cancel-button" onClick={onClose}>
+          {/* <button type="button" className="cancel-button" onClick={onClose}>
             Cancel
-          </button>
+          </button> */}
         </form>
       </div>
     </div>
