2c78834e6a719b6f4cecca06ffebc6251b17094b
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-01-07 23:26:28 +0200

    feat: add CreateWorkspaceForm component to Workspace for workspace creation

--- frontend/src/sidenavcomponents/workspace/Workspace.js
+++ frontend/src/sidenavcomponents/workspace/Workspace.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Workspace.css';
+import CreateWorkspaceForm from './components/createworkspaceform/CreateWorkspaceForm';
 
 const Workspace = () => {
     return (
         <div className="workspace">
             <h1>Workspace</h1>
             <p>Welcome to your workspace!</p>
+
+            <CreateWorkspaceForm />
         </div>
+        
     );
 };
 
