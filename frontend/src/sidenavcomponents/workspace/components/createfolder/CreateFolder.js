0dcb880cbe8aeeb1c014ef26114675b26b31470e
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-01-23 20:12:39 +0200

    style: add CreateFolder component with associated CSS for layout

---
+++ frontend/src/sidenavcomponents/workspace/components/createfolder/CreateFolder.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateFolder = () => {
+  return (
+    <div className="create-folder">
+      <h1>Create Folder</h1>
+      
+    </div>
+  );
+};
+
+export default CreateFolder;
