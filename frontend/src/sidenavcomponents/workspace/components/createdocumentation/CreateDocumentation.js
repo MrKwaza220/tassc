a0084e8b597a62424cec42cea49ddf5ecb38ead7
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-01-23 20:12:27 +0200

    style: add CreateDocumentation component with associated CSS for layout

---
+++ frontend/src/sidenavcomponents/workspace/components/createdocumentation/CreateDocumentation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateDocumentation = () => {
+  return (
+    <div className="create-documentation">
+      <h1>Create Documentation</h1>
+    </div>
+  );
+};
+
+export default CreateDocumentation;
