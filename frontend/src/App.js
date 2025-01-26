158b3bef0a8b11df8b25d176ce1f1ebff89e1311
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-01-26 22:26:30 +0200

    style: format code and enhance readability in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/navbar/NavBar';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Footer from './components/footer/Footer';
-import Dashboard from './pages/dashboard/Dashboard';
-import AuthForm from './pages/authform/AuthForm';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Footer from "./components/footer/Footer";
+import Dashboard from "./pages/dashboard/Dashboard";
+import AuthForm from "./pages/authform/AuthForm";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        
         <Routes>
-          <Route path="/" element={
-            <>
-            <NavBar />
-            <Home />
-            <About />
-            </>
-            } 
-            />
-          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <Home />
+                <About />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <> 
+                <NavBar />
+                <About />
+              </>
+            }
+          />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/authform" element={<AuthForm />} />
-          
-          
         </Routes>
-        
+
         <Footer />
       </div>
     </Router>
