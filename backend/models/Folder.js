4af6c5dd07e75e9bf2efeceea7f36ad9087cd491
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-05 22:50:34 +0200

    refactor: update Folder model schema for consistency in property naming

--- backend/models/Folder.js
+++ backend/models/Folder.js
@@ -1,9 +1,15 @@
-//models/Folders.js
 const mongoose = require("mongoose");
 
 const FolderSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    Workspace: {type: mongoose.Schema.Types.ObjectId, ref: "Workspace", required: true},
+  name: {
+    type: String,
+    required: true,
+  },
+  workspace: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Workspace",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Folder", FolderSchema);
