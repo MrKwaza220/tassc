3e33d4a50dc33df850ad7786406b91e8028ab05c
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-06 20:32:36 +0200

    feat: add projectTasks route to server for project task management

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,8 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/workspaces', require('./routes/WorkspaceRoutes'));
 app.use("/api/folders", require("./routes/folderRoutes"));
+app.use("/api/projectTasks", require("./routes/projectTaskRoutes"));
+
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
